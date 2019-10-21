a60a9a37b703548dc30d8839528c45cb6e67cab0
Mihai Neagu <mihai.neagu@emag.ro>
2019-10-22 00:09:53 +0300

    [technical_exercise] Pass the ChessBoard tests related to positioning.

--- ChessProject-PHP/src/ChessBoard.php
+++ ChessProject-PHP/src/ChessBoard.php
@@ -4,19 +4,47 @@ namespace SolarWinds\Chess;
 
 class ChessBoard
 {
-    const MAX_BOARD_WIDTH = 7;
-    const MAX_BOARD_HEIGHT = 7;
+    const MAX_BOARD_WIDTH = 8;
+    const MAX_BOARD_HEIGHT = 8;
 
     private $pieces;
 
+    private $piecesCount = [];
+
     public function __construct()
     {
+        $this->piecesCount[PieceColorEnum::WHITE()->innerValue()] = 0;
+        $this->piecesCount[PieceColorEnum::BLACK()->innerValue()] = 0;
         $this->pieces = array_fill(0, self::MAX_BOARD_WIDTH, array_fill(0, self::MAX_BOARD_HEIGHT, 0));
     }
 
     public function add(Pawn $pawn, $xCoordinate, $yCoordinate, PieceColorEnum $pieceColor)
     {
-        throw new \ErrorException("Need to implement " . __METHOD__);
+        if (!empty($this->pieces[$xCoordinate][$yCoordinate])) {
+            $pawn->setXCoordinate(-1);
+            $pawn->setYCoordinate(-1);
+
+            return;
+        }
+
+        $this->piecesCount[$pawn->getPieceColor()->innerValue()]++;
+
+        if ($this->maxNumberOfPiecesExceeded($pawn->getPieceColor())) {
+            $pawn->setXCoordinate(-1);
+            $pawn->setYCoordinate(-1);
+
+            return;
+        }
+
+        $pawn->setXCoordinate($xCoordinate);
+        $pawn->setYCoordinate($yCoordinate);
+
+        $this->pieces[$xCoordinate][$yCoordinate] = $pawn;
+    }
+
+    private function maxNumberOfPiecesExceeded(PieceColorEnum $pieceColor)
+    {
+        return $this->piecesCount[$pieceColor->innerValue()] >= 2 * self::MAX_BOARD_WIDTH;
     }
 
     /**
@@ -24,6 +52,7 @@ class ChessBoard
  	 **/
     public function isLegalBoardPosition($xCoordinate, $yCoordinate)
     {
-        throw new \ErrorException("Need to implement " . __METHOD__);
+        return $xCoordinate >= 0 && $yCoordinate >= 0
+            && $xCoordinate < self::MAX_BOARD_HEIGHT && $yCoordinate < self::MAX_BOARD_WIDTH;
     }
 }
