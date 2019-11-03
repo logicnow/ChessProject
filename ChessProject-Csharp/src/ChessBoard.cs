16e84868d4bb9de08aadfe340ceaed58f2c19116
Luis Santos <luisfcs@hotmail.co.uk>
2019-11-03 18:31:40 +0000

    Added ChessBoard::IsLegalBoardPosition() logic

--- ChessProject-Csharp/src/ChessBoard.cs
+++ ChessProject-Csharp/src/ChessBoard.cs
@@ -4,9 +4,9 @@ namespace SolarWinds.MSP.Chess
 {
     public class ChessBoard
     {
-        public static readonly int MaxBoardWidth = 7;
+        public static readonly int MaxBoardWidth = 7; // 8x8 2D array
         public static readonly int MaxBoardHeight = 7;
-        private Pawn[,] pieces;
+        private Pawn[,] pieces; 
 
         public ChessBoard ()
         {
@@ -20,7 +20,15 @@ namespace SolarWinds.MSP.Chess
 
         public bool IsLegalBoardPosition(int xCoordinate, int yCoordinate)
         {
-            throw new NotImplementedException("Need to implement ChessBoard.IsLegalBoardPosition()");
+            if (xCoordinate < 0 || yCoordinate < 0)
+            {
+                if (xCoordinate > MaxBoardWidth + 1 || yCoordinate > MaxBoardHeight + 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
     }
